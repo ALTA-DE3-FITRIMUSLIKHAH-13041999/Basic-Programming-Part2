56c7668b8f45fb0feb34798d697ece7e4818819f
fitri <fitrimuslikhah7@gmail.com>
2024-05-05 19:53:39 +0700

    problem 2

--- problem2/main.py
+++ problem2/main.py
@@ -4,9 +4,8 @@ tinggi = 25.0
 
 
 # kode disini
-phi = 3.14
 alas = 20.0
 tinggi = 25.0
-luassegitiga = (3.14 * 20.0 * 25.0)
+luassegitiga = (1/2 * 20.0 * 25.0)
 print("Luas Segitiga = ", round(luassegitiga, 2));
 
